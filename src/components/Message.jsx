0647c8375f1980267ec51dad582370df72bf9f84
Matthieu <matthieunadeau96@gmail.com>
2018-09-23 17:52:54 -0400

    Implemented logic of Message component and passed props to MessageList

--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -1,7 +1,16 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const Message = ({}) => (
-  <div>Message</div>
-);
+class Message extends Component {
+
+  render() {
+    return (
+      <div className="message">
+        <div className="message-username">{this.props.username}</div>
+        <div className="message-text">{this.props.text}</div>
+      </div>
+    );
+  }
+
+}
 
 export default Message;
--- src/components/MessageList.jsx
+++ src/components/MessageList.jsx
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 
+import Message from './Message';
+
 class MessageList extends Component {
   render() {
     return (
@@ -7,10 +9,7 @@ class MessageList extends Component {
         {
           this.props.messages.map((message, index) => {
             return (
-              <div key={index} className="message">
-                <div className="message-username">{message.senderId}</div>
-                <div className="message-text">{message.text}</div>
-              </div>
+              <Message key={index} username={message.senderId} text={message.text} />
             )
           })
         }
