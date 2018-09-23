73bdc42c5b8fed4b2c1aa0fbab8d16281dbd2926
Matthieu <matthieunadeau96@gmail.com>
2018-09-23 18:04:20 -0400

    Added a Form and Input to SendMessageForm and added some styling

--- src/components/SendMessageForm.jsx
+++ src/components/SendMessageForm.jsx
@@ -1,7 +1,11 @@
 import React from 'react';
 
 const SendMessageForm = ({}) => (
-  <div className="sendMessageForm child">SendMessageForm</div>
+  <div className="sendMessageForm child">
+    <form>
+      <input type="text" placeholder="Enter your message here..."/>
+    </form>
+  </div>
 );
 
 export default SendMessageForm;
