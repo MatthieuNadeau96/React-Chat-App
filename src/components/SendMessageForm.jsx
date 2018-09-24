11d6b513421b74c77b78dfbf2d1eef0ebf0c0265
Matthieu <matthieunadeau96@gmail.com>
2018-09-24 19:28:26 -0400

    refactored SendMessageForm to handle state and added handleChange function

--- src/components/SendMessageForm.jsx
+++ src/components/SendMessageForm.jsx
@@ -1,13 +1,29 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const SendMessageForm = ({}) => (
-  <form className="sendMessageForm child">
-    <input
-      onChange={this.handleChange }
-      type="text"
-      placeholder="Type your message and press ENTER"
-    />
-  </form>
-);
+class SendMessageForm extends Component {
+
+  state = {
+    message: ''
+  }
+
+  handleChange = (e) => {
+    this.setState({message: e.target.value})
+  }
+
+  render() {
+    console.log(this.state.message)
+    return (
+      <form className="sendMessageForm child">
+        <input
+          onChange={this.handleChange}
+          value={this.state.message}
+          type="text"
+          placeholder="Type your message and press ENTER"
+        />
+      </form>
+    );
+  }
+
+}
 
 export default SendMessageForm;
