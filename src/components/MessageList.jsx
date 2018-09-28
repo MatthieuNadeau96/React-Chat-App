672d8b7dc5f5372340d57e38c02ab210ba1a7c0a
Matthieu <matthieunadeau96@gmail.com>
2018-09-28 11:17:11 -0400

    Added a todo for the scrollDownButton

--- src/components/MessageList.jsx
+++ src/components/MessageList.jsx
@@ -2,9 +2,10 @@ import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import Message from './Message';
 
+
 class MessageList extends Component {
 
-  componentWillUpdate() { 
+  componentWillUpdate() {
     const node = ReactDOM.findDOMNode(this);
     this.shouldScrollToBottom = node.scrollTop + node.clientHeight + 200 >= node.scrollHeight;
   }
@@ -15,6 +16,8 @@ class MessageList extends Component {
       node.scrollTop = node.scrollHeight;
     }
   }
+
+  // TODO: Add a button that scrolls you to the bottom of the messageList when there is a new message to see
 
   render() {
     return (
