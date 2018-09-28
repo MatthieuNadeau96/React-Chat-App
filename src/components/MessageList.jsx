bbadba40a256a95bd4e5fdb0d79e2de4a142bf28
Matthieu <matthieunadeau96@gmail.com>
2018-09-28 10:10:27 -0400

    Added componentDidUpdate and ReactDOM to MessageList component to scroll to the bottom of the page when the component updates

--- src/components/MessageList.jsx
+++ src/components/MessageList.jsx
@@ -1,8 +1,14 @@
 import React, { Component } from 'react';
-
+import ReactDOM from 'react-dom';
 import Message from './Message';
 
 class MessageList extends Component {
+
+  componentDidUpdate() {
+    const node = ReactDOM.findDOMNode(this)
+    node.scrollTop = node.scrollHeight
+  }
+
   render() {
     return (
       <div className="messageList child">
