feb20c3c9144be497b943136b94d6b14a43d001e
Matthieu <matthieunadeau96@gmail.com>
2018-09-29 08:49:56 -0400

    removed unused code to remove minor console errors

--- src/App.js
+++ src/App.js
@@ -8,7 +8,6 @@ import MessageList from './components/Messaging/MessageList';
 import SendMessageForm from './components/Messaging/SendMessageForm';
 import NavBar from './components/Navigation/NavBar';
 import Backdrop from './components/Navigation/Backdrop.jsx';
-import ToggleRoomButton from './components/Navigation/ToggleRoomButton';
 import SideDrawer from './components/Navigation/SideDrawer';
 
 import { tokenUrl, instanceLocator } from './config';
