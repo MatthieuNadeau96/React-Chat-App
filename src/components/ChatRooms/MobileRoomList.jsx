ee7aaa32cc5ee5dfdb843eb1276a1574b7e3c5d9
Matthieu <matthieunadeau96@gmail.com>
2018-10-25 21:29:09 -0400

    Added Material-ui Button component to the friends button inside the MobileRoomList

--- src/components/ChatRooms/MobileRoomList.jsx
+++ src/components/ChatRooms/MobileRoomList.jsx
@@ -1,4 +1,16 @@
 import React, { Component } from 'react';
+import Button from '@material-ui/core/Button';
+
+const friends = {
+  display: 'block',
+  color: '#969CB6',
+  background: "inherit",
+  padding: 0,
+  height: 40,
+  width: 40,
+  borderRadius: "50%",
+  marginRight: 0
+}
 
 class MobileRoomList extends Component {
 
@@ -6,9 +18,11 @@ class MobileRoomList extends Component {
     const orderedRooms = [...this.props.rooms].sort((a, b) => a.id - b.id)
     return (
       <div className="mobileRoomList child">
-        <div className="mobileFriends">
-          <i className="fas fa-user-friends"></i>
-        </div>
+        <Button style={friends}>
+          <div className="mobileFriends">
+            <i className="fas fa-user-friends"></i>
+          </div>
+        </Button>
         <div className="room-title">Your Rooms</div>
         {orderedRooms.map(room => {
           const active = this.props.roomId === room.id ? "active" : "";
