009a41d9b757e13c86ffe4107d4c378e84af5592
Matthieu <matthieunadeau96@gmail.com>
2018-10-27 14:38:08 -0400

    Added more to Options component: added Sliders from material-ui/lab for option placeholders

--- src/components/Options/Options.jsx
+++ src/components/Options/Options.jsx
@@ -1,25 +1,82 @@
-import React from 'react';
+import React, { Component } from 'react';
 
+import { withStyles } from '@material-ui/core/styles';
 import Switch from '@material-ui/core/Switch';
+import Slider from '@material-ui/lab/Slider';
 
-const Options = props => (
-  <div className="options">
-    <div className="option">
-      <p>Dark Theme</p>
-      <Switch
-          color="primary"
-        />
-    </div>
-    <div className="option">
-      <p>Voice Volume</p>
-    </div>
-    <div className="option">
-      <p>Microphone Volume</p>
-    </div>
-    <div className="option">
-      <p>Font Size</p>
-    </div>
-  </div>
-);
+const styles = {
+  slider: {
+    padding: '2px 0px',
+  },
+}
 
-export default Options;
+class Options extends Component {
+
+  state = {
+    volumeValue: 50,
+    micValue: 100,
+    fontSizeValue: 2,
+  }
+
+  handleVolumeChange = (event, value) => {
+    this.setState({ volumeValue: value });
+  };
+  handleMicVolumeChange = (event, value) => {
+    this.setState({ micValue: value });
+  };
+  handleFontSizeChange = (event, value) => {
+    this.setState({ fontSizeValue: value });
+  };
+
+  render() {
+    return (
+      <div className="options">
+        <div className="option switch-option">
+          <p>Dark Theme</p>
+          <Switch
+              color="primary"
+            />
+        </div>
+        <div className="option slider-option">
+          <p id="label">Volume</p>
+          <Slider
+            className={{ container: styles.slider }}
+            value={this.state.volumeValue}
+            min={0}
+            max={100}
+            step={1}
+            aria-labelledby="label"
+            onChange={this.handleVolumeChange}
+            />
+        </div>
+        <div className="option slider-option">
+          <p id="label">Microphone Volume</p>
+            <Slider
+              className={{ container: styles.slider }}
+              value={this.state.micValue}
+              min={0}
+              max={100}
+              step={1}
+              aria-labelledby="label"
+              onChange={this.handleMicVolumeChange}
+              />
+        </div>
+        <div className="option slider-option">
+          <p>Font Size</p>
+            <Slider
+              className={{ container: styles.slider }}
+              value={this.state.fontSizeValue}
+              min={1}
+              max={3}
+              step={1}
+              aria-labelledby="label"
+              onChange={this.handleFontSizeChange}
+              />
+        </div>
+      </div>
+    );
+  }
+
+}
+
+export default withStyles(styles)(Options);
