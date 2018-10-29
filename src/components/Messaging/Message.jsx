e773f5a23fa9cfc4a64bcd7a9b1639c323ea3659
Matthieu <matthieunadeau96@gmail.com>
2018-10-29 11:13:40 -0400

    Implemented an Avatar from material-ui for the messages

--- src/components/Messaging/Message.jsx
+++ src/components/Messaging/Message.jsx
@@ -1,16 +1,29 @@
 import React, { Component } from 'react';
 
+import Avatar from '@material-ui/core/Avatar';
+
 class Message extends Component {
 
   render() {
     return (
       <div className="message">
         <div className="message-username">{this.props.username}</div>
-        <div className="message-text">{this.props.text}</div>
+        <div className="message-nameDisplay">
+          <Avatar style={avatar}>T</Avatar>
+          <div className="message-text">{this.props.text}</div>
+        </div>
       </div>
     );
   }
+}
 
+const avatar = {
+  margin: 6,
+  height: 30,
+  width: 30,
+  fontSize: '1em',
+  color: '#fff',
+  backgroundColor: '#FF9B9B'
 }
 
 export default Message;
