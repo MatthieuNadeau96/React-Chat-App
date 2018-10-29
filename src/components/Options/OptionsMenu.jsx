3668045674eb1ba58599c4f51098c41419c84643
Matthieu <matthieunadeau96@gmail.com>
2018-10-29 10:10:41 -0400

    Linked google's material-icons, added a back button to the OptionsMenu, and gave the back button some styling

--- src/components/Options/OptionsMenu.jsx
+++ src/components/Options/OptionsMenu.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import Options from './Options';
+import Button from '@material-ui/core/Button';
 
 const OptionsMenu = props => {
   let menuClasses = ['optionsMenu'];
@@ -11,10 +12,25 @@ const OptionsMenu = props => {
 
   return (
     <div className={menuClasses.join(' ')}>
+      <Button style={backBtn}><i class="material-icons">
+arrow_back
+</i></Button>
       <h2 className="optionMenuTitle">Options</h2>
       <Options />
     </div>
   )
+}
+
+const backBtn = {
+  display: 'block',
+  background: "inherit",
+  color: "#969CB6",
+  padding: 0,
+  height: 40,
+  width: 40,
+  fontSize: 25,
+  marginLeft: 20,
+  borderRadius: "50%"
 }
 
 export default OptionsMenu;
